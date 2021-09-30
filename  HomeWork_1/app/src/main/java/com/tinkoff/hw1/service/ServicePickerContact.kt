c3c69f91c63bf07f28631ca976226af7090d112a
ponomaREG <ponomarcomru@gmail.com>
2021-09-30 13:08:35 +0300

    Add services

---
+++  HomeWork_1/app/src/main/java/com/tinkoff/hw1/service/ServicePickerContact.kt
@@ -0,0 +1,50 @@
+package com.tinkoff.hw1.service
+
+import android.annotation.SuppressLint
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.provider.ContactsContract
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.tinkoff.hw1.util.Constant
+import com.tinkoff.hw1.model.Contact
+
+class ServicePickerContact : Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val users = loadContacts()
+        val intentBroadcast = Intent(Constant.ACTION_GET_CONTACTS)
+                .putParcelableArrayListExtra(Constant.EXTRA_KEY_CONTACTS, users)
+        val broadcastManager = LocalBroadcastManager.getInstance(baseContext)
+        broadcastManager.sendBroadcast(intentBroadcast)
+        stopSelf()
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? = null
+
+    @SuppressLint("Range")
+    private fun loadContacts(): ArrayList<Contact>{
+        val userContacts = arrayListOf<Contact>()
+        val uri = ContactsContract.Contacts.CONTENT_URI
+        val cursor = contentResolver.query(
+                uri,
+                null,
+                null,
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME
+        )
+        cursor?.moveToFirst()
+        while(cursor?.moveToNext() == true){
+            val displayName =
+                    cursor.getString(
+                            cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+                    )
+            userContacts.add(
+                    Contact(contactName = displayName)
+            )
+        }
+        cursor?.close()
+        return userContacts
+    }
+}
