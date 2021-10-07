e4808d10c7594c0d9e57b43582220e5d33b7e807
ponomaREG <ponomarcomru@gmail.com>
2021-10-07 18:10:03 +0300

    Show example of MessageViewGroup

--- CourseWork/app/src/main/java/com/tinkoff/coursework/MainActivity.kt
+++ CourseWork/app/src/main/java/com/tinkoff/coursework/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.tinkoff.coursework
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tinkoff.coursework.model.Message
+import com.tinkoff.coursework.model.Reaction
+import com.tinkoff.coursework.view.MessageViewGroup
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<MessageViewGroup>(R.id.ex_message)
+                .setMessage(mockMessage())
     }
+
+    private fun mockReactions() = listOf(
+            Reaction(Random.nextInt(0x1F300, 0x1f310), Random.nextInt(1, 200)),
+            Reaction(Random.nextInt(0x1F300, 0x1f310), Random.nextInt(1, 200)),
+            Reaction(Random.nextInt(0x1F300, 0x1f310), Random.nextInt(1, 200)),
+            Reaction(Random.nextInt(0x1F300, 0x1f310), Random.nextInt(1, 200)),
+            Reaction(Random.nextInt(0x1F300, 0x1f310), Random.nextInt(1, 200)),
+            Reaction(Random.nextInt(0x1F300, 0x1f310), Random.nextInt(1, 200)),
+            Reaction(Random.nextInt(0x1F300, 0x1f310), Random.nextInt(1, 200)),
+            Reaction(Random.nextInt(0x1F300, 0x1f310), Random.nextInt(1, 200)),
+    )
+
+    private fun mockMessage() = Message(
+            "Иванов Иван",
+            "Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/естовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/естовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/естовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/естовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/Тестовое сообщение/ \n" +
+                    " Конец ",
+            R.mipmap.ic_launcher,
+            mockReactions()
+    )
 }
