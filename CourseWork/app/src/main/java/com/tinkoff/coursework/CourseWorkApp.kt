323f334be5360a270965ce10d0245513e034c7a3
ponomaREG <ponomarcomru@gmail.com>
2021-11-13 02:19:42 +0300

    Add resources

--- CourseWork/app/src/main/java/com/tinkoff/coursework/CourseWorkApp.kt
+++ CourseWork/app/src/main/java/com/tinkoff/coursework/CourseWorkApp.kt
@@ -1,7 +1,19 @@
 package com.tinkoff.coursework
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class CourseWorkApp : Application()
+class CourseWorkApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
+}
